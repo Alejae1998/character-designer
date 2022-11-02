600120f234a9bf14b7fb866bfbd272e30a475e60
Alejae1998 <alejagr1998@gmail.com>
2022-11-02 11:00:09 -0700

    middleDropdown slice done

--- app.js
+++ app.js
@@ -30,9 +30,13 @@ headDropdown.addEventListener('change', () => {
 
 middleDropdown.addEventListener('change', () => {
   // get the value of the middle dropdown
+  console.log('clicking middle', middleDropdown.value);
   // increment the middle change count state
+  middle++;
   // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
+  middleEl.style.backgroundImage = `url('./assets/${middleDropdown.value}-middle.png')`;
   // update the stats to show the new count (call displayStats() to do this work)
+  displayStats();
 });
 
 bottomDropdown.addEventListener('change', () => {
